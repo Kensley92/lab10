7fbdd446b0568188983c3c9a3684ee8fee627ac1
Kensley92 <72991374+Kensley92@users.noreply.github.com>
2021-04-02 16:06:49 -0400

    lab 10

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
+import NameSearch from "./components/NameSearch";
+import ReportingSearch from "./components/ReportingArea";
+import ScoreSearch from "./components/ScoreSearch";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <NameSearch/>
+      <ScoreSearch/>
+      <ReportingArea/>
     </div>
   );
 }
--- client/src/components/ScoreSearch/index.js
+++ client/src/components/ScoreSearch/index.js
@@ -1,5 +1,45 @@
 import React from 'react';
 
 class ScoreSearch extends React.Component {
-    
+    readScore(event) {
+
+        // Stop (prevent)
+        event.preventDefault();
+
+        // Find the id=score
+        let element = document.querySelector("#score");
+
+        fetch("/api/movies/score/" + element.value)
+        .then((res) => {
+            // Parse the string
+            return res.json();
+        })
+        .then((processed) => {
+
+            // Find the id=reportingArea
+            let reporting = document.querySelector("#ReportingArea");
+
+            // Does the processed object
+            if(processed.error) {
+                reporting.innerHTML = processed.error;
+            } else {
+                reporting.innerHTML = processed.name;
+            }
+        });
+        element.value="";
+    }
+
+    render() {
+        return (
+                <div>
+                    <h2>Score</h2>
+                    <form onSubmit={this.readScore}>
+                    <input id="score" type="text"/>
+                    <button>Submit</button>
+                    </form>
+                </div>
+        )
+    }
 }
+
+export default ScoreSearch;
